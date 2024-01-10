ddefecede02627ab083866bb7d085d1000c1cb6d
gabr1234iel <106829590+gabr1234iel@users.noreply.github.com>
2024-01-10 20:00:29 +0800

    commit

--- my-app/App/Screen/LoginScreen/LoginScreen.jsx
+++ my-app/App/Screen/LoginScreen/LoginScreen.jsx
@@ -1,64 +1,52 @@
+import React, { useCallback } from 'react';
 import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
-import React from 'react';
 import FontAwesome from 'react-native-vector-icons/FontAwesome'; // Import FontAwesome
 import * as WebBrowser from "expo-web-browser";
-import * as Facebook from 'expo-auth-session/providers/facebook';
 import { useWarmUpBrowser } from "../../../hooks/useWarmUpBrowser";
 import { useOAuth } from "@clerk/clerk-expo";
-
 
 WebBrowser.maybeCompleteAuthSession();
 
 export default function LoginScreen() {
 
     useWarmUpBrowser();
- 
-    const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
-    const onPress = React.useCallback(async () => {
-        try {
-          const { createdSessionId, signIn, signUp, setActive } =
-            await startOAuthFlow();
-     
-          if (createdSessionId) {
-            setActive({ session: createdSessionId });
-          } else {
-            // Use signIn or signUp for next steps such as MFA
-          }
-        } catch (err) {
-          console.error("OAuth error", err);
+    // Google Auth
+    const { startOAuthFlow } = useOAuth({ strategy: 'oauth_google' });
+    const handleGoogleLogin = useCallback(async () => {
+      try {
+        const { createdSessionId, setActive } = await startOAuthFlow();
+        if (createdSessionId) {
+          setActive({ session: createdSessionId });
         }
-      }, []);
-     
+      } catch (err) {
+        console.error('OAuth error', err);
+      }
+    }, []);
 
-  return (
-    <View style={styles.container}>
-        <Image
-            source={require('../../../assets/images/hmdyb-logo.png')}
-            style={styles.logoImage}
-        />
-        <Text style={styles.titleText}>How Much Do You Bench</Text>
+    return (
+        <View style={styles.container}>
+            <Image
+                source={require('../../../assets/images/hmdyb-logo.png')}
+                style={styles.logoImage}
+            />
+            <Text style={styles.titleText}>How Much Do You Bench</Text>
 
-        {/* Google Login Button */}
-        <TouchableOpacity style={[styles.authButton, { backgroundColor: '#DB4437' }]} onPress={onPress}>
-            <FontAwesome name="google" style={styles.authIcon} />
-            <Text style={styles.authButtonText}>Login with Google</Text>
-        </TouchableOpacity>
-
-        {/* Facebook Login Button */}
-        <TouchableOpacity style={[styles.authButton, { backgroundColor: '#3B5998' }]} onPress={() => { /* Handle Facebook login logic here */ }}>
-            <FontAwesome name="facebook" style={styles.authIcon} />
-            <Text style={styles.authButtonText}>Login with Facebook</Text>
-        </TouchableOpacity>
-    </View>
-  );
+            {/* Google Login Button */}
+            <TouchableOpacity style={[styles.authButton, { backgroundColor: '#DB4437' }]} onPress={handleGoogleLogin}>
+                <FontAwesome name="google" style={styles.authIcon} />
+                <Text style={styles.authButtonText}>Login with Google</Text>
+            </TouchableOpacity>
+        </View>
+    );
 }
+
 const styles = StyleSheet.create({
     container: {
       flex: 1,
       justifyContent: 'center',
       alignItems: 'center',
       padding: 20,
-      backgroundColor: '#fff',
+      backgroundColor: '#f7f7f7',
     },
     logoImage: {
       width: 300,
@@ -74,8 +62,8 @@ const styles = StyleSheet.create({
     authButton: {
       flexDirection: 'row',
       alignItems: 'center',
-      justifyContent: 'flex-start', // Align items to the start
-      backgroundColor: '#4285F4',
+      justifyContent: 'flex-start',
+      backgroundColor: '#DB4437',
       paddingHorizontal: 20,
       paddingVertical: 10,
       borderRadius: 20,
@@ -85,12 +73,6 @@ const styles = StyleSheet.create({
       shadowOffset: { height: 0, width: 0 },
       width: '80%',
       marginBottom: 15,
-    },
-    buttonTextContainer: {
-      flex: 1, // Take up all available space
-      alignItems: 'center', // Align text to the center
-      justifyContent: 'center', // Center text vertically
-      marginLeft: -20, // Adjust this value to center the text as per your visual requirement
     },
     authButtonText: {
       color: '#fff',
