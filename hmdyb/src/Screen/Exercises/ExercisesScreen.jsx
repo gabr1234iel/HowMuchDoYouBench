6ca3f87686bb266e1a6b9c3c41c44935f8b80263
gabr1234iel <106829590+gabr1234iel@users.noreply.github.com>
2024-01-15 21:24:54 +0800

    ts implementation

--- hmdyb/src/Screen/Exercises/ExercisesScreen.jsx
+++ hmdyb/src/Screen/Exercises/ExercisesScreen.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import TitleComponent from '../../Components/TitleComponent';
+import firestore from '@react-native-firebase/firestore';
 
 
 export default function ExercisesScreen() {
+
+  const [ exercise, setExercise ] = useState('');
+  const [ part, setPart ] = useState('');
+  const ref = firestore().collection('exercises');
+
+  async function addExercise() {
+    await ref.add({
+      title: exercise,
+      part: part,
+    });
+    setExercise('');
+    setPart('');
+  }
+
+
   return (
     <View style={styles.container}>
       <TitleComponent title="Exercises" />
-      {/* Rest of your screen content */}
+      <TextInput label={'New Exercise'} value={exercise} onChangeText={setExercise} />
+      <TextInput label={'Part'} value={part} onChangeText={setPart} />
+      <Button onPress={() => addExercise()}>Add EXERCISE</Button>
     </View>
   );
 }
